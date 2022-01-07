7eb02718633193f598ade25c408b2c65c63d5cd3
Gaurav Kumar <phibinary@gmail.com>
2022-01-07 23:23:03 +0530

    completed

---
+++ src/components/UI/ModalOverlay.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from "./ModalOverlay.module.css";
+import Card from "./Card";
+import Button from "./Button";
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+export default ModalOverlay;
