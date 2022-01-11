d1b0bc43d27ae3957e7b917283b8f0325be700fb
Gaurav Kumar <phibinary@gmail.com>
2022-01-11 22:07:26 +0530

    cleaning

--- src/components/UI/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "./Backdrop.module.css";
-
-const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onConfirm} />;
-};
-
-export default Backdrop;
